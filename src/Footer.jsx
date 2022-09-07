0c1609d34081d6f893d3114e7275a7c164716fe6
Nishant Kumar Jha <62249082+NKJ-C4@users.noreply.github.com>
2022-09-07 22:01:03 +0530

    Updated github profile link in footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () =>{
     return (
         <>
         <footer className="w-100 bg-light py-2">
-            <p className="text-center">Made with ❤️ by Nishant</p>
+            <p className="text-center">Made with ❤️ by <a href="https://www.github.com/nkj-c4/" target="_blank">Nishant</a></p>
         </footer>
         </>
     );
